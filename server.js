ac60dde610b446b3216b8e3b0266224af0a8082c
pvptowers <paultowers2005@yahoo.com.au>
2021-04-06 21:26:23 +1000

    updated server.js to handle unhandled rejection errors

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ dotenv.config({
 //DEFINE ENVIRONMENT PORT
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+//START & LISTEN TO SERVER
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION.... Shutting down");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
