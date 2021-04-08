4b0493a4ccc5641620469c9b21772cf4c3740277
pvptowers <paultowers2005@yahoo.com.au>
2021-04-08 22:58:11 +1000

    created updateTeam controller and route

--- components/teams/team.controller.js
+++ components/teams/team.controller.js
@@ -2,9 +2,71 @@ const mongoose = require("mongoose");
 const asyncHandler = require("../../middleware/asyncHandler");
 const Team = require("../../models/team.model");
 const authenticatedToken = require("../../middleware/authenticatedToken");
+const ErrorResponse = require("../../utils/errorResponse");
 
 exports.getTeam = asyncHandler(async (req, res, next) => {
   const team = await Team.findById(req.params._id);
+  if (!team) {
+    return next(new ErrorResponse("No Team Exists With This ID"));
+  }
+  res.status(200).json({
+    success: true,
+    data: team,
+  });
+});
+
+// exports.updateTeam = asyncHandler(async (req, res, next) => {
+//   const team = await Team.findById(req.params._id, (err, doc) => {
+//     if (err) {
+//       return next(new ErrorResponse("Unable to Update Team"));
+//     } else {
+//       doc.teamName = req.body.teamName;
+//       doc.save();
+//     }
+//     res.status(200).json({
+//       success: true,
+//       data: doc,
+//     });
+//   });
+// });
+
+// exports.updateTeam = asyncHandler(async (req, res, next) => {
+//   const team = await Team.findById(req.params._id, (err, doc) => {
+//     if (err) {
+//       return next(new ErrorResponse("eror"));
+//     } else if (!doc) {
+//       return next(new ErrorResponse("No Team Exists With This ID"));
+//     } else {
+//       doc.teamName = req.body.teamName;
+//       doc.save();
+//     }
+//     res.status(200).json({
+//       success: true,
+//       data: doc,
+//     });
+//   });
+// });
+
+exports.updateTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.findById(req.params._id);
+  console.log(team);
+  if (!team) {
+    return next(new ErrorResponse("No Team Exists with this ID"));
+  }
+  if (!req.body.teamName) {
+    return next(new ErrorResponse("You need to provide a teamName"));
+  } else {
+    team.teamName = req.body.teamName;
+    const newteam = await team.save();
+    res.status(200).json({
+      success: true,
+      data: newteam,
+    });
+  }
+});
+
+exports.deleteTeam = asyncHandler(async (req, res, next) => {
+  const team = await Team.findByIdAndDelete(req.params._id);
   res.status(200).json({
     success: true,
     data: team,
--- components/teams/team.route.js
+++ components/teams/team.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getTeam } = require("./team.controller");
+const { getTeam, updateTeam, deleteTeam } = require("./team.controller");
 
 const router = express.Router();
 
 router.get(`/get-team/:_id`, getTeam);
+router.put(`/update-team/:_id`, updateTeam);
+router.delete(`/delete-team/:_id`, deleteTeam);
 
 module.exports = router;
