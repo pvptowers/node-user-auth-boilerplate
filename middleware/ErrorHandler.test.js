404f3694f2309bde6c9933588c94570603579851
pvptowers <paultowers2005@yahoo.com.au>
2021-06-06 21:09:41 +1000

    testing change

--- middleware/ErrorHandler.test.js
+++ middleware/ErrorHandler.test.js
@@ -151,7 +151,7 @@ describe("Errors", () => {
     };
   });
 
-  it("throws an error aaa", async () => {
+  it("throws an error aatfegra", async () => {
     ErrorHandler(error, mReq, mRes, mNext);
     console.log("THIS IS ERRR", error);
     console.log("This is res", mRes);
