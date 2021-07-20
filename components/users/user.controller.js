3278bab023f3df45c3b69d28824c94dd5ed5847d
pvptowers <paultowers2005@yahoo.com.au>
2021-07-20 23:03:45 +1000

    created deleteUser and updateUser controller and route

--- components/users/user.controller.js
+++ components/users/user.controller.js
@@ -2,9 +2,37 @@ const mongoose = require("mongoose");
 const asyncHandler = require("../../middleware/asyncHandler");
 const User = require("../../models/user.model");
 const authenticatedToken = require("../../middleware/authenticatedToken");
+const ErrorResponse = require("../../middleware/errorResponse");
 
 exports.getUser = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.params._id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndRemove(req.params._id);
+  if (!user) {
+    return next(new ErrorResponse("No user exists with that id", 404));
+  }
+  res.status(200).json({
+    success: true,
+    data: null,
+  });
+});
+
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params._id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) {
+    return next(new ErrorResponse("No user exists with that id", 404));
+  }
+
   res.status(200).json({
     success: true,
     data: user,
--- components/users/user.route.js
+++ components/users/user.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getUser } = require("./user.controller");
+const { getUser, deleteUser, updateUser } = require("./user.controller");
 
 const router = express.Router();
 
-router.get(`/get-user/:_id`, getUser);
+router.get("/get-user/:_id", getUser);
+router.delete("/delete-user/:_id", deleteUser);
+router.patch("/update-user/:_id", updateUser);
 
 module.exports = router;
