5926f773af0e490132fa2532044898922455fc1a
pvptowers <paultowers2005@yahoo.com.au>
2021-07-29 22:27:20 +1000

    added protected route functionality

--- components/authentication/authentication.controller.js
+++ components/authentication/authentication.controller.js
@@ -2,6 +2,9 @@ const asyncHandler = require("../../middleware/asyncHandler");
 const authenticatedToken = require("../../middleware/authenticatedToken");
 const authService = require("./authentication.service");
 const tokenService = require("./token.service");
+const User = require("../../models/user.model");
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
 
 // DESCRIPTION: CREATE A NEW ACCOUNT & ROOT USER
 // ROUTE: POST /auth/create-account
@@ -62,4 +65,46 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
     req.body.passwordConfirm
   );
   authenticatedToken(user, 200, res);
+});
+
+// DESCRIPTION: Protect Middleware Method to ensure user is logged in when accessing specific routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  //GET TOKEN & CHECK IT EXISTS
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
+  //IF NO TOKEN RETURN ERROR MESSAGE
+  if (!token) {
+    return next(new ErrorResponse("Please log in to access", 401));
+  }
+  //VERIFY TOKEN WITH JWT
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  //CHECK IF USER STILL EXISTS
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new ErrorResponse(
+        "The user belonging to this token no longer exists",
+        401
+      )
+    );
+  }
+
+  //CHECK IF USER HAS CHANGED PASSWORD AFTER THE TOKEN WAS ISSUED
+  // if (currentUser.changedPasswordAfter(decoded.iat)) {
+  //     return next(new ErrorResponse("User recently changed password, please log in again", 401))
+  // }
+
+  //GRANT ACCESS TO THE PROTECTED ROUTE
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
 });
--- components/teams/team.route.js
+++ components/teams/team.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { protect } = require("../authentication/authentication.controller");
 const {
   getTeam,
   addUser,
@@ -8,7 +8,7 @@ const {
 } = require("./team.controller");
 const router = express.Router();
 
-router.get(`/get-team/:_id`, getTeam);
+router.get(`/get-team/:_id`, protect, getTeam);
 router.put(`/update-team/:_id`, updateTeam);
 router.delete(`/delete-team/:_id`, deleteTeam);
 router.post("/add-user", addUser);
